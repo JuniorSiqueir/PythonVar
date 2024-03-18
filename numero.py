f46b3772a2f8bc8ef47b266ede1272cde4734254
Anderson Junior <92762017+JuniorSiqueir@users.noreply.github.com>
2024-03-18 10:47:55 -0300

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,43 @@
+# Inteiro
+numero_inteiro = 3
+print("Inteiro = ", numero_inteiro)
+
+# Real com ponto flutuante
+numero_real = 3.12
+print("Real com ponto flutuante = ", numero_real)
+
+# type
+print("Tipo da variavel = ", type(numero_inteiro))
+print("Tipo da variavel = ", type(numero_real))
+
+# soma +
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+# subtração -
+subtracao = 1 - 1
+print("1 - 1 = ", subtracao)
+
+# multiplicação *
+multi = 2 * 2
+print("2 x 2 = ", multi)
+
+# divisão /
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variavel = ", type(divisao))
+
+# int
+print("Valor em inteiro = ", int(divisao))
+
+# float
+print("Valor em float = ", float(soma))
+
+# Modulo %
+modulo = 5 % 2
+print("Modulo = ", modulo)
+
+# divisão //
+divisaoInt = 5 // 2
+print("5 / 2 = ", divisaoInt)
+print("Tipo da variavel = ", type(divisaoInt))
